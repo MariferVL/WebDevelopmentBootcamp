cdb66ddd0b576fede8e20224ad49057c543c70f1
Marifer VL <mfvl.1186@gmail.com>
2022-12-19 11:05:33 -0300

    finished

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,10 +1,27 @@
+var players = [];
+const player1 = document.getElementById("img1");
+const player2 = document.getElementById("img2");
+var dice1;
+var dice2;
+
 function spinDice() {
     for (i = 1; i < 3; i++) {
         var diceNum = Math.random();
-        diceNum = Math.floor(diceNum * 6);
-        console.log(diceNum);
-        document.getElementsByClassName("img" + i).src = "/images/dice${diceNum}.png";
-        }
+        diceNum = Math.floor(diceNum * 6) + 1;
+        players.push(diceNum);
+    }
+    dice1 = "images/dice" + players[0] + ".png";
+    dice2 = "images/dice" + players[1] + ".png";
+    player1.setAttribute("src", dice1);
+    player2.setAttribute("src", dice2);
+    if (dice1 > dice2) {
+        document.querySelector("h1").innerHTML = "🏁 Player 1 Wins! "
+    } else if (dice2 > dice1) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🏁"
+    } else {
+        document.querySelector("h1").innerHTML = "Draw!"
+    }
+
 }
 
-document.onload = spinDice();
+location.reload = spinDice(); 
